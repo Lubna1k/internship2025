5a8aa274fdb9fd7e7b333ef37d4e0dab46a91592
Lubna1k <lubnamansoorkhan@gmail.com>
2025-09-02 22:26:53 -0400

    automation-2025-internship/python-selenium-automation-2025-main/features/environment.py

--- automation-2025-internship/python-selenium-automation-2025-main/features/steps/first_inter.py
+++ automation-2025-internship/python-selenium-automation-2025-main/features/steps/first_inter.py
@@ -3,10 +3,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class Page:
 
+
  def __init__(self, driver):
-    self.driver = driver
-    self.base_url = 'https://soft.reelly.io'
-    self.wait = WebDriverWait(self.driver, timeout=10)
+  self.driver = driver
+  self.base_url = 'https://soft.reelly.io'
+  self.wait = WebDriverWait(self.driver, timeout=10)
 
 def open_url(self, url):
     self.driver.get(url)
@@ -29,8 +30,7 @@ def wait_until_clickable(self, *locator):
   message=f'Element not clickable by {locator}')
 
 def wait_until_clickable_click(self, *locator):
-    self.wait.until(
-    EC.element_to_be_clickable(locator),
+    self.wait.until (EC.element_to_be_clickable(locator),
     message=f'Element not clickable by {locator}').click()
 
 def wait_until_visible(self, *locator):
