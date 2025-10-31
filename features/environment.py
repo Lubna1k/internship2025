c5936bc21983fb74dc00ea98214e70d7a24f67f2
Lubna1k <lubnamansoorkhan@gmail.com>
2025-10-31 12:51:41 -0400

    features/environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,13 @@
+from allure_behave.utils import scenario_name
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 
+from selenium.webdriver.chrome.options import Options
 from app.application import Application
+
+
 
 
 def browser_init(context):
@@ -17,7 +21,7 @@ def browser_init(context):
     # context.driver = webdriver.Chrome(service=service)
 
     # Firefox Browser
-    context.driver = webdriver.Firefox()
+    #context.driver = webdriver.Firefox()
 
     # Headless Mode
     # options = webdriver.ChromeOptions()
@@ -25,7 +29,21 @@ def browser_init(context):
     # context.driver =  webdriver.Chrome(
     #     options=options
     # )
+   #Browser Stack
+    bs_user = 'lubnakh_8yCX9o'
+    bs_key = 'ycPPy2YByhzqkPgGvx3R'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
 
+    options = Options()
+    bstack_options = {
+        "os": "windows",
+        "osVersion": "10",
+        "browserVersion": "latest",
+        'browserName': 'chrome',
+        'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
     driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
     context.driver = webdriver.Chrome(service=service)
@@ -36,9 +54,12 @@ def browser_init(context):
     context.app = Application(context.driver)
 
 
+
+
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
+
 
 
 def before_step(context, step):
